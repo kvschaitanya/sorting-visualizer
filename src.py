4d0f9bf9047323f2752ca1a984852e8108561d85
Chaitanya Nalluri <kvschaitanyanalluri25@gmail.com>
2024-09-25 04:10:41 +0530

    added refresh function

--- src.py
+++ src.py
@@ -2,16 +2,22 @@ import pygame
 import random
 
 pygame.init()
+pygame.font.init()
 
-BACKGROUND = (37, 37, 37)
-BAR = (0, 143, 148)
-HIGHLIGHTED_BAR = (105, 48, 195)
-COMPLETED_BAR = (57, 255, 20)
+calibri18 = pygame.font.SysFont('calibri', 18, True)
+calibri18.set_underline(True)
+calibri16 = pygame.font.SysFont('calibri', 16, True)
+
+BACKGROUND = [37, 37, 37]
+BAR = [0, 143, 148]
+HIGHLIGHTED_BAR = [105, 48, 195]
+COMPLETED_BAR = [57, 255, 20]
 
 arr_size = 40 # default, 100 = huge, 80 = big, 40 = medium, 20 = small, 10 = very small
 delay_time = 20 # 0 = instant, 10 = fast, 20 = normal, 30 = slow, 40 = very slow
 # 0 = Bubble Sort, 1 = Selection Sort, 2 = Quick Sort, 3 = Merge Sort, 4 = Heap Sort
 color_index = [BAR for _ in range(arr_size)]
+line_width = (1200 - (arr_size - 1) * 5 - 5) // arr_size
 circle_pos = (((20, 540),
           (20, 565), 
           (20, 590), 
@@ -40,8 +46,6 @@ screen.fill(BACKGROUND)
 pygame.display.flip()
 
 arr = [random.randint(10, 500) for _ in range(arr_size)]
-line_width = (1200 - (arr_size - 1) * 5 - 5) // arr_size
-print('Line width =', line_width, 'Pixels')
 
 def draw_bars(arr:list):
     screen.fill(BACKGROUND, (0, 0, 1200, 500))
@@ -60,7 +64,29 @@ def draw_circles():
     
     pygame.draw.circle(screen, (20, 20, 20), circle_pos[0][2], 4)
     pygame.draw.circle(screen, (20, 20, 20), circle_pos[1][2], 4)
-    pygame.draw.circle(screen, (20, 20, 20), circle_pos[2][2], 4)
+    pygame.draw.circle(screen, (20, 20, 20), circle_pos[2][0], 4)
+
+    screen.blit(calibri18.render("Array Size:", True, (240, 240, 240)), (5, 505))
+    screen.blit(calibri18.render("Speed:", True, (240, 240, 240)), (185, 505))
+    screen.blit(calibri18.render("Algorithm:", True, (240, 240, 240)), (365, 505))
+
+    screen.blit(calibri16.render("Huge", True, (240, 240, 240)), (35, 532))
+    screen.blit(calibri16.render("Big", True, (240, 240, 240)), (35, 557))
+    screen.blit(calibri16.render("Medium", True, (240, 240, 240)), (35, 582))
+    screen.blit(calibri16.render("Small", True, (240, 240, 240)), (35, 607))
+    screen.blit(calibri16.render("Very Small", True, (240, 240, 240)), (35, 632))
+
+    screen.blit(calibri16.render("Very Quick", True, (240, 240, 240)), (215, 532))
+    screen.blit(calibri16.render("Fast", True, (240, 240, 240)), (215, 557))
+    screen.blit(calibri16.render("Normal", True, (240, 240, 240)), (215, 582))
+    screen.blit(calibri16.render("Slow", True, (240, 240, 240)), (215, 607))
+    screen.blit(calibri16.render("Very Slow", True, (240, 240, 240)), (215, 632))
+
+    screen.blit(calibri16.render("Bubble Sort", True, (240, 240, 240)), (395, 532))
+    screen.blit(calibri16.render("Selection Sort", True, (240, 240, 240)), (395, 557))
+    screen.blit(calibri16.render("Quick Sort", True, (240, 240, 240)), (395, 582))
+    screen.blit(calibri16.render("Merge Sort", True, (240, 240, 240)), (395, 607))
+    screen.blit(calibri16.render("Heap Sort", True, (240, 240, 240)), (395, 632))
 
     pygame.display.update()
 
@@ -74,6 +100,7 @@ def mark_circle(marked_pos):
                         pygame.draw.circle(screen, (192, 192, 192), circle_pos[0][circle_val[0].index(arr_size)], 7)
                         arr_size = circle_val[index][i]
                         pygame.draw.circle(screen, (20, 20, 20), pos, 4)
+                        refresh()
 
                     elif index == 1:
                         pygame.draw.circle(screen, (192, 192, 192), circle_pos[1][circle_val[1].index(delay_time)], 7)
@@ -82,6 +109,13 @@ def mark_circle(marked_pos):
                     # else:
                     #     pass
     pygame.display.update()
+
+def refresh():
+    global color_index, arr, line_width
+    arr = [random.randint(10, 500) for _ in range(arr_size)]
+    line_width = (1200 - (arr_size - 1) * 5 - 5) // arr_size
+    color_index = [BAR for _ in range(arr_size)]
+    draw_bars(arr)
 
 def bubblesort(arr:list):
     for i in range(len(arr) - 1, -1, -1):
@@ -107,8 +141,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.WINDOWCLOSE:
             running = False
-        elif event.type == pygame.KEYDOWN and event.dict['key'] == pygame.K_s:         # Event(768-KeyDown {'unicode': 's', 'key': 115, 'mod': 0, 'scancode': 22, 'window': None})
-            bubblesort(arr)
+        elif event.type == pygame.KEYDOWN:         # Event(768-KeyDown {'unicode': 's', 'key': 115, 'mod': 0, 'scancode': 22, 'window': None})
+            if event.dict['key'] == pygame.K_s:
+                bubblesort(arr)
+            elif event.dict['key'] == pygame.K_r:
+                refresh()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # if option:
             #     pass
